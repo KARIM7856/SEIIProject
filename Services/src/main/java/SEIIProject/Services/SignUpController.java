444453ecc202168a65640735ad53614f9432fb3c
omar hassan <36304190+o3m7a7r@users.noreply.github.com>
2020-04-09 16:23:39 +0200

    .

--- Services/src/main/java/SEIIProject/Services/SignUpController.java
+++ Services/src/main/java/SEIIProject/Services/SignUpController.java
@@ -1,5 +1,7 @@
 package SEIIProject.Services;
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpRequest;
@@ -8,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import SEIIProject.Model.*;
+import SEIIProject.Repository.DataRepository;
+import SEIIProject.Repository.Loader;
+import SEIIProject.Repository.UserLoader;
 
 @RestController
 public class SignUpController {
@@ -16,20 +21,22 @@ public class SignUpController {
 	String signUp(HttpServletRequest rep) {
 		
 		AbstractUser user;
-		String email = rep.getParameter("email");
 		String firstName = rep.getParameter("firstName");
 		String lastName = rep.getParameter("lastName");
+		String userName = rep.getParameter("userName");
 		String password = rep.getParameter("password");
-		String type = rep.getParameter("Type");
+		String email = rep.getParameter("email");
+		String type = rep.getParameter("type");
 		
 		
 		
 		user = UserFactory.getUser(type);
-		user.setEmail(email);
 		user.setFirstName(firstName);
 		user.setLastName(lastName);
+		user.setUserName(userName);
 		user.setPassword(password);
-		
+		user.setEmail(email);
+		user.setType(type);
 		user.saveToDatabase();
 		
 		return user.toString();
@@ -37,6 +44,13 @@ public class SignUpController {
 		
 	}
 	
-	
+		
+		@RequestMapping("/printAll")
+		public ArrayList<AbstractUser> printAll() {
+			Loader l = new UserLoader();
+			//ArrayList<AbstractUser> au = new ArrayList();
+			//AbstractUser u = null;
+			return l.loadALL();
+		}
 
 }
